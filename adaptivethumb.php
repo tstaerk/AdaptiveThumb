3d66389347ae65a35a0527702b14741fe67c36ea
Thorsten Staerk <dev@staerk.de>
2015-01-17 14:31:30 +0000

    allow a caption

--- adaptivethumb.php
+++ adaptivethumb.php
@@ -15,9 +15,23 @@ function pichtml($code, $argv)
   $param1=reset($argv); # get first value of array
   $param2=next($argv);
   $param3=next($argv);
-  $result="<img src=$param1 width=$param2 align=$param3 />";
-  //$result=implode($argv);
-  //$result="result ".$argv[0];
+  $param4=next($argv);
+  $result="<div style=\"border:1px;solid;color=\"cccccc\">
+<table width=$param2 border=1 align=$param3>
+<tr border=0>
+<td style=\"border:0px\">
+<img src=$param1 width=100% />
+</td>
+</tr>
+<tr>
+<td style=\"border:0px\" align=center>
+$param4
+</td>
+</tr>
+</table>
+</div>
+";
+  $result=preg_replace("/\n/","",$result);
   return $result;
 }
 
