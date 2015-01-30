4bd7f97a54126dd23dd87e45aa6b18ed3f5d353a
Thorsten Staerk <dev@staerk.de>
2015-01-30 08:20:22 +0100

    allowing picture without table border around it, patch by Fabian

--- adaptivethumb.php
+++ adaptivethumb.php
@@ -16,23 +16,20 @@ function pichtml($code, $argv)
   $width=htmlentities($argv['width']); // needn't be numeric, could be "50%"
   $src=htmlentities($argv['src']);
   $caption=htmlentities($argv['caption']);
+  $mode=htmlentities($argv['mode']);
   if (empty($align)) {$align="right";};
   if (empty($width)) {$width="100%";};
-  $result="<div style=\"border:1px;solid;color=\"cccccc\">
-<table width=$width border=1 align=$align>
-<tr border=0>
-<td style=\"border:0px\">
-<img src=$src width=100% />
-</td>
-</tr>
-<tr>
-<td style=\"border:0px\" align=center>
-$caption
-</td>
-</tr>
-</table>
-</div>
-";
+  if ($mode == "table") {
+    $mytable1="<table width=$width border=2 align=$align>
+      <tr border=0><td style=\"border:0px\">";
+    $mytable2="</td><tr><td style=\"border:0px\" align=center>
+      $caption</td></tr></table>";
+    $myimage="<img src=$src width=100% />";
+  } else {
+    $mytable1=""; $mytable2="";
+    $myimage="<img src=$src width=$width align=$align />";
+  }
+  $result="<div style=\"border:1px;solid;color=\"cccccc\"> $mytable1 $myimage $mytable2 </div>";
   $result=preg_replace("/\n/","",$result);
   return $result;
 }
