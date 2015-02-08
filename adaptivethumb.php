70e78e175523002c7950b175d90d18588cce3b84
Thorsten Staerk <dev@staerk.de>
2015-02-08 18:16:59 +0000

    Allow alt and title. See issue 1: https://github.com/tstaerk/adaptivethumb/issues/1.

--- adaptivethumb.php
+++ adaptivethumb.php
@@ -26,10 +26,13 @@ class picextension
     $caption=htmlentities($argv['caption']);
     $border=htmlentities($argv['border']);
     $link=htmlentities($argv['link']);
+    $title=htmlentities($argv['title']);
+    $alt=htmlentities($argv['alt']);
     if (!empty($link)) {$linkopen="<a href=$link>"; $linkclose="</a>";};
     if (empty($align)) {$align="right";};
     if (empty($width)) {$width="100%";};
     if (!is_numeric($border)) {$border=0;};
+    $myimage="<img src=$src title=$title alt=$alt align=$align />";
     if (!empty($caption)) 
     {
       $parsedcaption=$parser->parse($caption, $parser->mTitle, $parser->mOptions, false, false);
@@ -38,12 +41,11 @@ class picextension
         <tr border=0><td style=\"border:0px\">"; // table rows do not have an extra border
       $tableclose="</td><tr><td style=\"border:0px\" align=center> 
 		".$parsedcaptiontext."</td></tr></table>"; // table cells do not have an extra border
-      $myimage="<img src=$src width=100% />"; // the table width is already scaled down so the image width must be 100%
+      $myimage="<img src=$src width=100% title=$title />"; // the table width is already scaled down so the image width must be 100%
     } 
     else 
     {
       $tableopen=""; $tableclose="";
-      $myimage="<img src=$src width=$width align=$align />";
     }
     $result="$tableopen$linkopen$myimage$linkclose$tableclose";
     $result=preg_replace("/\n/","",$result);
